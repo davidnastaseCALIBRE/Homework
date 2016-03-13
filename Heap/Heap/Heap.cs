7d0f4085b1831a368be4884d71310eafc2a83a7f
David Nastase <davidwnastase@gmail.com>
2016-03-13 19:18:14 -0400

    Initial Commit for Jake's Homework assignment.
    Still need to finish unit tests and a README file and maybe some documentation on the methods.

---
+++ Heap/Heap/Heap.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeapAssignment
+{
+    public class Heap
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Heap()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="add"></param>
+        public void Insert(int add)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public int PeakMax()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public int ExtractMax()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public int Size()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="other"></param>
+        public void Merge(Heap other)
+        {
+
+        }
+    }
+}
---
+++ Heap/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using HeapAssignment;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [ExpectedException(typeof(System.NullReferenceException))]
+        public void HeapDefaultCharacteristics()
+        {
+            Heap test = new Heap();
+            Assert.AreEqual(0, test.Size());
+
+            test.Size(); //Should throw an exception.
+        }
+
+        [TestClass]
+        public void HeapInsert()
+        {
+            
+        }
+
+        [TestClass]
+        public void meh()
+        {
+
+        }
+    }
+}
